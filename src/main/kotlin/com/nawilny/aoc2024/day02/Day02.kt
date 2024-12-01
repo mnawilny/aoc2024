f4769bff8930df5142c5e08316c7d167b5aaf4ad
Michal Nawilny <mnawilny@gmail.com>
2024-12-01 21:14:24 +0100

    Cleanup

---
+++ src/main/kotlin/com/nawilny/aoc2024/day02/Day02.kt
@@ -0,0 +1,10 @@
+package com.nawilny.aoc2024.day02
+
+import com.nawilny.aoc2024.commons.Input
+import com.nawilny.aoc2024.commons.Input.println
+
+fun main() {
+    val input = Input.readFileLinesNormalized("day02", "example.txt")
+    input.println()
+}
+
